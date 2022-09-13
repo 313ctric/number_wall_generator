b4aa2b564c3177854f977ea99da1db29da2a760d
Electric <76964455+313ctric@users.noreply.github.com>
2022-09-13 21:05:29 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,3 @@ pub mod bi_directional_wall;
 pub mod left_const_wall;
 pub mod repeating_sequence_wall;
 
-// #[cfg(test)]
-// mod tests {
-//     #[test]
-//     fn it_works() {
-//         let result = 2 + 2;
-//         assert_eq!(result, 4);
-//     }
-// }
-
-
